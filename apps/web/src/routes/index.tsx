c72eb736630e647d791766b39cd11dddedd2baea
juaniwk3 <86119976+JuanIWK3@users.noreply.github.com>
2025-05-20 17:46:56 -0300

    feat(web): use textarea

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -2,13 +2,13 @@ import { Button } from "@/components/ui/button";
 import {
 	Form,
 	FormControl,
-	FormDescription,
 	FormField,
 	FormItem,
 	FormLabel,
 	FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createFileRoute } from "@tanstack/react-router";
 import { useForm } from "react-hook-form";
@@ -20,7 +20,7 @@ export const Route = createFileRoute("/")({
 
 const formSchema = z.object({
 	description: z.string().min(2, {
-		message: "Username must be at least 2 characters.",
+		message: "Description must be at least 20 characters.",
 	}),
 });
 
@@ -47,13 +47,19 @@ function Home() {
 						name="description"
 						render={({ field }) => (
 							<FormItem>
-								<FormLabel>Username</FormLabel>
+								<FormLabel>Job description</FormLabel>
 								<FormControl>
-									<Input placeholder="shadcn" {...field} />
+									<Textarea
+										placeholder="Company is a solution provider that aims to revolutionize the accounts payable department of traditional businesses in Brazil. By automating payment processes, Company offers efficiency, time savings, and convenience to entrepreneurs. Through a simple platform, clients can make one payment a day and have Company take care of the rest, including handling boletos, PIX, DARF, and more.
+Role Description
+This is a full-time remote role for a Full Stack Engineer at Company. The Full Stack Engineer will be responsible for both front-end and back-end web development tasks.
+Current Stack
+Node.js
+React
+Phyton"
+										{...field}
+									/>
 								</FormControl>
-								<FormDescription>
-									This is your public display name.
-								</FormDescription>
 								<FormMessage />
 							</FormItem>
 						)}
